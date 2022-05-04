5d3617f947500797395003b39e9330cf1a04abae
Martin <mwgoff24@students.bmchs.org>
2022-05-04 10:05:37 -0500

    finished lab 5.01

---
+++ Lab_5.01.py
@@ -0,0 +1,68 @@
+'''
+###########
+Lab 5.01
+###########
+
+Instructions
+------------
+Write a program that uses a dictionary to offer users the meanings of common internet abbreviations.
+
+The program, dictionary_lab.py, prompts the user to enter an internet abbreviation they would like explained. 
+
+If the requested abbreviation is in the program's dictionary (use the in keyword with an if statement 
+to test this), then it prints out the definition. If the abbreviation is not in the dictionary, the program 
+prints an apologetic message saying that it could not find a definition.
+
+Example Output:
+---------------
+>>> python3 dictionary_lab.py
+
+What word would you like to look up? nbd
+nbd: a phrase meaning no big deal
+
+What word would you like to look up? kittens
+Sorry, kittens is not defined
+
+What word would you like to look up?
+
+Bonus
+------
+Extend the program with any of these features:
+The user can
+update the definitions (values) for existing abbreviations in the dictionary
+add new abbreviations (keys) and provide their definitions (values).
+delete entries (key, value pairs) from the dictionary.
+get the entire dictionary printed to the screen.
+Lesson 6.01 did not cover all the techniques for manipulating dictionaries that you will need to program these features. 
+Search for the necessary information in the [Python tutorial about dictionaries][1] and the [advanced Python documentation about dictionaries][2].
+'''
+# internet abbreviations dictionary
+
+# dictionary containing 10 abbreviations found on the internet
+abbreviations_dictionary = {
+    "nbd": "stands for 'no big deal'",
+    "brb": "stands for 'be right back'",
+    "lol": "stands for 'laugh(ing) out loud'",
+    "rn": "stands for 'right now'",
+    "asap": "stands for 'as soon as possible'",
+    "fyi": "stands for 'for your information'",
+    "imo": "stands for 'in my opinion'",
+    "nvm": "stands for 'never mind'",
+    "smh": "stands for 'shaking my head'",
+    "ftw": "stands for 'for the win'"
+}
+# additional instructions before loop
+print("In addition to the abbreviations, you can also type 'quit' to exit the program or type 'print' to show the full dictionary. \n")
+
+# program loop
+while True:
+    abr = input("What is the abbreviation you would like to know or the command you would like to do?. ")
+    if abr == 'quit':
+        break
+    elif abr == 'print':
+        for item in abbreviations_dictionary:
+            print(f"{item}: {abbreviations_dictionary[item]}")
+    elif abr in abbreviations_dictionary:
+        print(f"{abbreviations_dictionary[abr]} \n")
+    else:
+        print("I'm sorry, but that is unavailable. Please try another. \n")
